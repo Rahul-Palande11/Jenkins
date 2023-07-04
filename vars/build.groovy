c921a8c44bd7310c07205326a47d3b97cc28f9cb
Rahul-Palande11 <131943341+Rahul-Palande11@users.noreply.github.com>
2023-07-04 10:12:12 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,11 +1,7 @@
 def call() {
-      stages {
-           stage('build') {
-                 steps {
+      
                        bat "javac Hello.java"
                        bat "java Hello"
                    echo "Building done"
-                 }
-           }
-      }
+    
 }
