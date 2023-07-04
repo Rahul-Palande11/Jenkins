1764c28aafd72fc2abd4d933be05071246fbc195
Rahul-Palande11 <131943341+Rahul-Palande11@users.noreply.github.com>
2023-07-04 10:12:32 +0530

    Update cloning.groovy

--- vars/cloning.groovy
+++ vars/cloning.groovy
@@ -1,10 +1,6 @@
 def call(String Url, String branch) {
-        stages {
-                stage('clonning') {
-                      steps {
+       
                               git url: "${Url}", branch: "${branch}"
                         echo "Clonning done"
-                      }
-                }
-        }
+      
 }
